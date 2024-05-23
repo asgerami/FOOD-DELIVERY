613a090e0268e9e0ba0448a4e56690e4ea801e7b
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-23 22:43:47 +0300

    Create Admin Dashboard: Admin Dashboard where the admin can see the statistics like catagories

---
+++ admin/index.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>Food Delivery Website - Hoem Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <div class="menu">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Catagory</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+        </div>
+
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">All RIghts Reserved</p>
+            </div>
+        </div>
+        </body>
+</html>
