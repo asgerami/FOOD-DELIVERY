bef91ebab7658527d94723e743a1ce95d551fc47
Amir <96184499+asgerami@users.noreply.github.com>
2024-05-25 09:57:43 +0300

    Added different pages of admin panels

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,13 +1,58 @@
 <?php include('partials/menu.php'); ?>
 
+
         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Admin</h1>
+                <br /><br />
 
-                
-                <div class="clearfix"></div>
+
+                <a href="#" class="btn-primary">Add Admin</a>
+
+                <br /><br /><br />
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1. </td>
+                        <td>Amir Aman</td>
+                        <td>amiraman</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2. </td>
+                        <td>Abduselam Mohammednur</td>
+                        <td>abduselam</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3. </td>
+                        <td>Abudurahman Sualih</td>
+                        <td>abdurahman</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
+
 
             </div>
         </div>
+
         <?php include('partials/footer.php'); ?>
     
