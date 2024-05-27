df5c6ddbb5c84de7a605cc5a936fe7bda090ab2c
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2024-05-27 12:44:30 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,14 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Dashboard</h1>
+                 <br><br>
+                 <?php 
+            if(isset($_SESSION['login'])) 
+            {
+                echo $_SESSION['login'];
+                unset($_SESSION['login']); 
+            }
+            ?>
 
                 <div class="col-4 text-center">
                     <h1>5</h1>
