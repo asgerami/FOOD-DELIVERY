e7d57f9583c1a739cbd058b48373490a971f50e1
Amir <96184499+asgerami@users.noreply.github.com>
2024-06-27 02:40:57 +0300

    Update menu.php

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -1,18 +1,26 @@
-<?php include('../config/constants.php'); ?>
+<?php 
+
+    include('../config/constants.php'); 
+    include('login-check.php');
+
+?>
+
 <html>
     <head>
-        <title>Food Delivery Website - Hoem Page</title>
+        <title>Food Order Website - Home Page</title>
         <link rel="stylesheet" href="../css/admin.css">
     </head>
+    
     <body>
         <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="manage-admin.php">Admin</a></li>
-                <li><a href="manage-catagory.php">Catagory</a></li>
-                <li><a href="manage-food.php">Food</a></li>
-                <li><a href="manage-order.php">Order</a></li>
-            </ul>
-        </div>
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-food.php">Food</a></li>
+                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
         </div>
