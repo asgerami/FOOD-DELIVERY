b0490b56a29626950747bfddb6b631e83b7f4ac6
Amir <96184499+asgerami@users.noreply.github.com>
2024-06-27 02:49:14 +0300

    Update manage-food.php

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -1,55 +1,118 @@
 <?php include('partials/menu.php'); ?>
 
 <div class="main-content">
-<div class="wrapper">
-<h1>Manage Food</h1>
+    <div class="wrapper">
+        <h1>Manage Food</h1>
 
-<br /><br />
+        <br /><br />
 
+                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
 
-<a href="#" class="btn-primary">Add Food</a>
+                <br /><br /><br />
 
-<br /><br /><br />
+                <?php 
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add'];
+                        unset($_SESSION['add']);
+                    }
 
-<table class="tbl-full">
-    <tr>
-        <th>S.N.</th>
-        <th>Full Name</th>
-        <th>Username</th>
-        <th>Actions</th>
-    </tr>
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
 
-    <tr>
-        <td>1. </td>
-        <td>Amir Aman</td>
-        <td>amiraman</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+                    if(isset($_SESSION['upload']))
+                    {
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                    }
 
-    <tr>
-        <td>2. </td>
-        <td>Abduselam Mohammednur</td>
-        <td>abduselam</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+                    if(isset($_SESSION['unauthorize']))
+                    {
+                        echo $_SESSION['unauthorize'];
+                        unset($_SESSION['unauthorize']);
+                    }
 
-    <tr>
-        <td>3. </td>
-        <td>Abudurahman Sualih</td>
-        <td>abdurahman</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
-</table>
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+                
+                ?>
 
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <?php 
+                        $sql = "SELECT * FROM tbl_food";
+
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
+                        $sn=1;
+
+                        if($count>0)
+                        {
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id = $row['id'];
+                                $title = $row['title'];
+                                $price = $row['price'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $sn++; ?>. </td>
+                                    <td><?php echo $title; ?></td>
+                                    <td><?php echo $price; ?> Birr</td>
+                                    <td>
+                                        <?php  
+                                            if($image_name=="")
+                                            {
+                                                echo "<div class='error'>Image not Added.</div>";
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
+                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
+                                    </td>
+                                </tr>
+
+                                <?php
+                            }
+                        }
+                        else
+                        {
+                            echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
+                        }
+
+                    ?>
+
+                    
+                </table>
+    </div>
+    
 </div>
-</div>
+
 <?php include('partials/footer.php'); ?>
