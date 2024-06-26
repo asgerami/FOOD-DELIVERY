c0d1668f2a963a7159ca37709c0d5d963ee53c69
Amir <96184499+asgerami@users.noreply.github.com>
2024-06-27 02:50:03 +0300

    Update manage-order.php

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -1,51 +1,114 @@
 <?php include('partials/menu.php'); ?>
 
 <div class="main-content">
-<div class="wrapper">
-<h1>Manage Order</h1>
+    <div class="wrapper">
+        <h1>Manage Order</h1>
 
+                <br /><br /><br />
 
-<br /><br /><br />
+                <?php 
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+                ?>
+                <br><br>
 
-<table class="tbl-full">
-    <tr>
-        <th>S.N.</th>
-        <th>Full Name</th>
-        <th>Username</th>
-        <th>Actions</th>
-    </tr>
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Food</th>
+                        <th>Price</th>
+                        <th>Qty.</th>
+                        <th>Total</th>
+                        <th>Order Date</th>
+                        <th>Status</th>
+                        <th>Customer Name</th>
+                        <th>Contact</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
 
-    <tr>
-        <td>1. </td>
-        <td>Amir Aman</td>
-        <td>amiraman</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+                    <?php 
+                        $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; 
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
 
-    <tr>
-        <td>2. </td>
-        <td>Abduselam Mohammednur</td>
-        <td>abduselam</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+                        $sn = 1; 
 
-    <tr>
-        <td>3. </td>
-        <td>Abudurahman Sualih</td>
-        <td>abdurahman</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
-</table>
+                        if($count>0)
+                        {
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id = $row['id'];
+                                $food = $row['food'];
+                                $price = $row['price'];
+                                $qty = $row['qty'];
+                                $total = $row['total'];
+                                $order_date = $row['order_date'];
+                                $status = $row['status'];
+                                $customer_name = $row['customer_name'];
+                                $customer_contact = $row['customer_contact'];
+                                $customer_email = $row['customer_email'];
+                                $customer_address = $row['customer_address'];
+                                
+                                ?>
 
+                                    <tr>
+                                        <td><?php echo $sn++; ?>. </td>
+                                        <td><?php echo $food; ?></td>
+                                        <td><?php echo $price; ?></td>
+                                        <td><?php echo $qty; ?></td>
+                                        <td><?php echo $total; ?></td>
+                                        <td><?php echo $order_date; ?></td>
+
+                                        <td>
+                                            <?php 
+
+                                                if($status=="Ordered")
+                                                {
+                                                    echo "<label>$status</label>";
+                                                }
+                                                elseif($status=="On Delivery")
+                                                {
+                                                    echo "<label style='color: orange;'>$status</label>";
+                                                }
+                                                elseif($status=="Delivered")
+                                                {
+                                                    echo "<label style='color: green;'>$status</label>";
+                                                }
+                                                elseif($status=="Cancelled")
+                                                {
+                                                    echo "<label style='color: red;'>$status</label>";
+                                                }
+                                            ?>
+                                        </td>
+
+                                        <td><?php echo $customer_name; ?></td>
+                                        <td><?php echo $customer_contact; ?></td>
+                                        <td><?php echo $customer_email; ?></td>
+                                        <td><?php echo $customer_address; ?></td>
+                                        <td>
+                                            <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Order</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+
+                            }
+                        }
+                        else
+                        {
+                            echo "<tr><td colspan='12' class='error'>Orders not Available</td></tr>";
+                        }
+                    ?>
+
+ 
+                </table>
+    </div>
+    
 </div>
-</div>
+
 <?php include('partials/footer.php'); ?>
