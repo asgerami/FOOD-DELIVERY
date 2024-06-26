7ce8ba7a815c8000561e7c1937f77538b427d80b
Amir <96184499+asgerami@users.noreply.github.com>
2024-06-27 02:43:29 +0300

    Update add-admin.php

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -1,10 +1,11 @@
 <?php include('partials/menu.php'); ?>
 
-
 <div class="main-content">
     <div class="wrapper">
         <h1>Add Admin</h1>
-        <br /> <br/>
+
+        <br><br>
+
         <?php 
             if(isset($_SESSION['add'])) 
             {
@@ -13,42 +14,42 @@
             }
         ?>
 
-
         <form action="" method="POST">
 
-<table class="tbl-30">
-    <tr>
-        <td>Full Name: </td>
-        <td>
-            <input type="text" name="full_name" placeholder="Enter Your Name">
-        </td>
-    </tr>
+            <table class="tbl-30">
+                <tr>
+                    <td>Full Name: </td>
+                    <td>
+                        <input type="text" name="full_name" placeholder="Enter Your Name">
+                    </td>
+                </tr>
 
-    <tr>
-        <td>Username: </td>
-        <td>
-            <input type="text" name="username" placeholder="Your Username">
-        </td>
-    </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" placeholder="Your Username">
+                    </td>
+                </tr>
 
-    <tr>
-        <td>Password: </td>
-        <td>
-            <input type="password" name="password" placeholder="Your Password">
-        </td>
-    </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td>
+                        <input type="password" name="password" placeholder="Your Password">
+                    </td>
+                </tr>
 
-    <tr>
-        <td colspan="2">
-            <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
-        </td>
-    </tr>
-</table>
-</form>
-</div>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </div>
 
 <?php include('partials/footer.php'); ?>
+
 
 <?php 
    
@@ -63,24 +64,20 @@
             username='$username',
             password='$password'
         ";
+ 
        
-
         $res = mysqli_query($conn, $sql) or die(mysqli_error());
-
         if($res==TRUE)
         {
             $_SESSION['add'] = "<div class='success'>Admin Added Successfully.</div>";
-            //Redirect Page to Manage Admin
             header("location:".SITEURL.'admin/manage-admin.php');
         }
         else
         {
             $_SESSION['add'] = "<div class='error'>Failed to Add Admin.</div>";
-            //Redirect Page to Add Admin
             header("location:".SITEURL.'admin/add-admin.php');
         }
 
-
     }
-
+    
 ?>
