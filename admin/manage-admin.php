9565845e2586c97002518873bb1d211b4a1f6733
Amir <96184499+asgerami@users.noreply.github.com>
2024-06-27 02:47:25 +0300

    Update manage-admin.php

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,18 +4,48 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Admin</h1>
+
                 <br />
 
-                
-        <?php 
-            if(isset($_SESSION['add'])) 
-            {
-                echo $_SESSION['add'];
-                unset($_SESSION['add']); 
-            }
-        ?>
-        <br><br><br>
+                <?php 
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add']; 
+                        unset($_SESSION['add']);
+                    }
 
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+                    
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+
+                    if(isset($_SESSION['user-not-found']))
+                    {
+                        echo $_SESSION['user-not-found'];
+                        unset($_SESSION['user-not-found']);
+                    }
+
+                    if(isset($_SESSION['pwd-not-match']))
+                    {
+                        echo $_SESSION['pwd-not-match'];
+                        unset($_SESSION['pwd-not-match']);
+                    }
+
+                    if(isset($_SESSION['change-pwd']))
+                    {
+                        echo $_SESSION['change-pwd'];
+                        unset($_SESSION['change-pwd']);
+                    }
+
+                ?>
+                <br><br><br>
 
                 <a href="add-admin.php" class="btn-primary">Add Admin</a>
 
@@ -28,16 +58,16 @@
                         <th>Username</th>
                         <th>Actions</th>
                     </tr>
+
+                    
                     <?php 
                         $sql = "SELECT * FROM tbl_admin";
                         $res = mysqli_query($conn, $sql);
 
                         if($res==TRUE)
                         {
-
                             $count = mysqli_num_rows($res);
                             $sn=1;
-
                             if($count>0)
                             {
                                 while($rows=mysqli_fetch_assoc($res))
@@ -47,32 +77,35 @@
                                     $username=$rows['username'];
 
                                     ?>
+                                    
                                     <tr>
                                         <td><?php echo $sn++; ?>. </td>
                                         <td><?php echo $full_name; ?></td>
                                         <td><?php echo $username; ?></td>
                                         <td>
-                                            <a href="#" class="btn-secondary">Update Admin</a>
-                                            <a href="#" class="btn-danger">Delete Admin</a>
+                                            <a href="<?php echo SITEURL; ?>admin/update-password.php?id=<?php echo $id; ?>" class="btn-primary">Change Password</a>
+                                            <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
+                                            <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                                         </td>
                                     </tr>
+
                                     <?php
+
                                 }
                             }
                             else
                             {
                                 
                             }
-                    }
+                        }
+
                     ?>
 
 
-
-    
+                    
                 </table>
-
 
             </div>
         </div>
 
-        <?php include('partials/footer.php'); ?>
+<?php include('partials/footer.php'); ?>
